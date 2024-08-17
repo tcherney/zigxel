731e057f81760a91598eafd74347d109557e6265
Timothy Cherney <tjcherney@hotmail.com>
2024-08-16 17:59:20 -0700

    improved graphics rendering to enable color of square pixels instead of cursor sized rectangles

--- src/graphics.zig
+++ src/graphics.zig
@@ -1,19 +1,8 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const term = @import("term.zig");
 
-const PX = "█";
-
-//
-// fn initBuf(self: *Self) !void {
-//     const px_char_sz = px.len;
-//     const px_color_sz = BG[LAST_COLOR].len + FG[LAST_COLOR].len;
-//     const px_sz = px_color_sz + px_char_sz;
-//     const screen_sz: u64 = @as(u64, px_sz * self.size.width * self.size.width);
-//     const overflow_sz: u64 = px_char_sz * 100;
-//     const bs_sz: u64 = screen_sz + overflow_sz;
-//     self.buffer = try self.allocator.alloc(u8, bs_sz * 2);
-// }
+const PX = "▀";
+//▀█
 
 pub const Graphics = struct {
     ascii_based: bool = false,
@@ -25,12 +14,16 @@ pub const Graphics = struct {
     const Self = @This();
     pub fn init(allocator: std.mem.Allocator) !Graphics {
         const terminal = try term.Term.init(allocator);
+        var pixel_buffer = try allocator.alloc(u8, terminal.size.height * terminal.size.width * 2);
+        for (0..pixel_buffer.len) |i| {
+            pixel_buffer[i] = 0;
+        }
         return Self{
             .terminal = terminal,
             .allocator = allocator,
-            .pixel_buffer = try allocator.alloc(u8, terminal.size.height * terminal.size.width),
+            .pixel_buffer = pixel_buffer,
             // need space for setting background and setting of foreground color for every pixel
-            .terminal_buffer = try allocator.alloc(u8, (term.FG[term.LAST_COLOR].len + PX.len) * ((terminal.size.height * terminal.size.width) + 100)),
+            .terminal_buffer = try allocator.alloc(u8, (term.FG[term.LAST_COLOR].len + PX.len + term.BG[term.LAST_COLOR].len) * ((terminal.size.height * terminal.size.width) + 100)),
         };
     }
 
@@ -62,19 +55,42 @@ pub const Graphics = struct {
         try self.terminal.out(term.CURSOR_HOME);
         // fill terminal buffer with pixel colors
         var buffer_len: usize = 0;
-        var previous_pixel: u8 = 0;
+        var prev_fg_pixel: u8 = 0;
+        var prev_bg_pixel: u8 = 0;
+        var j: usize = 0;
+        var i: usize = 0;
+        const width = self.terminal.size.width;
+        const height = self.terminal.size.height * 2;
         // each pixel is an index into the possible 256 colors
-        for (self.pixel_buffer) |pixel| {
-            if (previous_pixel != pixel) {
-                previous_pixel = pixel;
-                for (term.FG[pixel]) |c| {
-                    self.terminal_buffer[buffer_len] = c;
-                    buffer_len += 1;
+        while (j < height) : (j += 2) {
+            i = 0;
+            while (i < width) : (i += 1) {
+                const fg_pixel = self.pixel_buffer[j * width + i];
+                const bg_pixel = self.pixel_buffer[(j + 1) * width + i];
+                if (prev_fg_pixel != fg_pixel) {
+                    prev_fg_pixel = fg_pixel;
+                    for (term.FG[fg_pixel]) |c| {
+                        self.terminal_buffer[buffer_len] = c;
+                        buffer_len += 1;
+                    }
                 }
-            }
-            for (PX) |c| {
-                self.terminal_buffer[buffer_len] = c;
-                buffer_len += 1;
+                if (prev_bg_pixel != bg_pixel) {
+                    prev_bg_pixel = bg_pixel;
+                    for (term.BG[bg_pixel]) |c| {
+                        self.terminal_buffer[buffer_len] = c;
+                        buffer_len += 1;
+                    }
+                }
+                // can render with space
+                if (fg_pixel == bg_pixel) {
+                    self.terminal_buffer[buffer_len] = ' ';
+                    buffer_len += 1;
+                } else {
+                    for (PX) |c| {
+                        self.terminal_buffer[buffer_len] = c;
+                        buffer_len += 1;
+                    }
+                }
             }
         }
         try self.terminal.out(self.terminal_buffer[0..buffer_len]);
@@ -88,7 +104,9 @@ test "square" {
     try graphics.set_bg(0, 0, 0);
     try graphics.draw_rect(50, 10, 5, 5, 255, 255, 0);
     try graphics.draw_rect(60, 8, 2, 3, 0, 255, 255);
-    try graphics.draw_rect(85, 8, 3, 1, 128, 75, 0);
+    try graphics.draw_rect(60, 8, 3, 1, 128, 75, 0);
+    try graphics.draw_rect(95, 15, 2, 1, 255, 128, 0);
+    try graphics.draw_rect(75, 10, 1, 1, 255, 128, 255);
     try graphics.flip();
     _ = try std.io.getStdIn().reader().readByte();
     try graphics.deinit();
