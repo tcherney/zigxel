3d9a6a447fc951e13b139c5f791a73b6c188357b
Timothy Cherney <tjcherney@hotmail.com>
2024-09-06 20:36:51 -0700

    cleaned up the update functions added density

--- src/physics_pixel.zig
+++ src/physics_pixel.zig
@@ -33,11 +33,15 @@ pub const PhysicsPixel = struct {
     pixel_type: PixelType,
     dirty: bool = false,
     last_dir: i32 = undefined,
+    density: f32,
+    solid: bool,
     const Self = @This();
     pub fn init(pixel_type: PixelType, x: i32, y: i32) Self {
         var r: u8 = undefined;
         var g: u8 = undefined;
         var b: u8 = undefined;
+        var density: f32 = undefined;
+        var solid: bool = undefined;
         switch (pixel_type) {
             .Sand => {
                 std.debug.print("sand color\n", .{});
@@ -46,6 +50,8 @@ pub const PhysicsPixel = struct {
                 g = @as(u8, @intCast(@as(u16, @bitCast(@as(i16, @bitCast(@as(u16, @intCast(SAND_COLOR.g)))) + variation))));
                 b = @as(u8, @intCast(@as(u16, @bitCast(@as(i16, @bitCast(@as(u16, @intCast(SAND_COLOR.b)))) + variation))));
                 std.debug.print("{d} {d} {d}\n", .{ r, g, b });
+                density = 3.0;
+                solid = true;
             },
             .Water => {
                 std.debug.print("water color\n", .{});
@@ -54,19 +60,25 @@ pub const PhysicsPixel = struct {
                 g = @as(u8, @intCast(@as(u16, @bitCast(@as(i16, @bitCast(@as(u16, @intCast(WATER_COLOR.g)))) + variation))));
                 b = @as(u8, @intCast(@as(u16, @bitCast(@as(i16, @bitCast(@as(u16, @intCast(WATER_COLOR.b)))) + variation))));
                 std.debug.print("{d} {d} {d}\n", .{ r, g, b });
+                density = 1.0;
+                solid = false;
             },
             .Empty => {
                 r = EMPTY_COLOR.r;
                 g = EMPTY_COLOR.g;
                 b = EMPTY_COLOR.b;
+                density = 0;
+                solid = false;
             },
             .Wall => {
                 r = WALL_COLOR.r;
                 g = WALL_COLOR.g;
                 b = WALL_COLOR.b;
+                density = 10.0;
+                solid = true;
             },
         }
-        return Self{ .x = x, .y = y, .pixel = Pixel{ .r = r, .g = g, .b = b }, .pixel_type = pixel_type, .last_dir = if (utils.rand.boolean()) -1 else 1 };
+        return Self{ .x = x, .y = y, .pixel = Pixel{ .r = r, .g = g, .b = b }, .pixel_type = pixel_type, .last_dir = if (utils.rand.boolean()) -1 else 1, .solid = solid, .density = density };
     }
 
     inline fn swap_pixel(self: *Self, pixels: []?*PhysicsPixel, x: i32, y: i32, xlimit: u32, _: u32) void {
@@ -93,31 +105,36 @@ pub const PhysicsPixel = struct {
     fn sand_update(self: *Self, pixels: []?*PhysicsPixel, xlimit: u32, ylimit: u32) void {
         const first = self.last_dir;
         const second = -first;
-        if (in_bounds(self.x, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x, self.y + 1, xlimit, ylimit);
-        } else if (in_bounds(self.x + first, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x + first, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x + first, self.y + 1, xlimit, ylimit);
-        } else if (in_bounds(self.x + second, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x + second, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x + second, self.y + 1, xlimit, ylimit);
+        if (self.execute_move(pixels, self.x, self.y + 1, xlimit, ylimit)) return;
+        if (self.execute_move(pixels, self.x + first, self.y + 1, xlimit, ylimit)) return;
+        if (self.execute_move(pixels, self.x + second, self.y + 1, xlimit, ylimit)) return;
+    }
+
+    inline fn execute_move(self: *Self, pixels: []?*PhysicsPixel, x: i32, y: i32, xlimit: u32, ylimit: u32) bool {
+        if (in_bounds(x, y, xlimit, ylimit)) {
+            const indx: u32 = @as(u32, @bitCast(y)) * xlimit + @as(u32, @bitCast(x));
+            if (!pixel_at_x_y(x, y, pixels, xlimit, ylimit)) {
+                self.swap_pixel(pixels, x, y, xlimit, ylimit);
+                return true;
+            } else if (!pixels[indx].?.solid and ((pixels[indx].?.density > self.density and y <= self.y) or
+                (pixels[indx].?.density < self.density and y > self.y)))
+            {
+                self.swap_pixel(pixels, x, y, xlimit, ylimit);
+                return true;
+            } else if (pixels[indx].?.density == self.density) {
+                self.last_dir = -self.last_dir;
+            }
         }
+        return false;
     }
 
     fn water_update(self: *Self, pixels: []?*PhysicsPixel, xlimit: u32, ylimit: u32) void {
         const first = self.last_dir;
         const second = -first;
-        if (in_bounds(self.x, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x, self.y + 1, xlimit, ylimit);
-        } else if (in_bounds(self.x + first, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x + first, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x + first, self.y + 1, xlimit, ylimit);
-        } else if (in_bounds(self.x + second, self.y + 1, xlimit, ylimit) and !pixel_at_x_y(self.x + second, self.y + 1, pixels, xlimit, ylimit)) {
-            self.swap_pixel(pixels, self.x + second, self.y + 1, xlimit, ylimit);
-        } else {
-            if (in_bounds(self.x + first, self.y, xlimit, ylimit) and !pixel_at_x_y(self.x + first, self.y, pixels, xlimit, ylimit)) {
-                self.swap_pixel(pixels, self.x + first, self.y, xlimit, ylimit);
-            } else {
-                self.last_dir = -self.last_dir;
-            }
-        }
+        if (self.execute_move(pixels, self.x, self.y + 1, xlimit, ylimit)) return;
+        if (self.execute_move(pixels, self.x + first, self.y + 1, xlimit, ylimit)) return;
+        if (self.execute_move(pixels, self.x + second, self.y + 1, xlimit, ylimit)) return;
+        if (!self.execute_move(pixels, self.x + first, self.y, xlimit, ylimit)) return;
     }
 
     // fn base_update(self: *Self, delta: u64, pixels: std.ArrayList(PhysicsPixel), xlimit: u32, ylimit: u32) void {
