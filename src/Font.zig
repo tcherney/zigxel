617e20515d0bc134518e8c1838ad686b40431d92
Timothy Cherney <tjcherney@hotmail.com>
2024-10-01 00:34:21 -0700

    more work on edges

--- src/Font.zig
+++ src/Font.zig
@@ -15,6 +15,16 @@ pub const Font = struct {
     const Edge = struct {
         p0: Point,
         p1: Point,
+        pub fn lessThan(_: void, lhs: Edge, rhs: Edge) bool {
+            const lhs_y_min = @min(lhs.p0.y, lhs.p1.y);
+            const rhs_y_min = @min(rhs.p0.y, rhs.p1.y);
+            if (lhs_y_min == rhs_y_min) {
+                const lhs_x_min = @min(lhs.p0.x, lhs.p1.x);
+                const rhs_x_min = @min(rhs.p0.x, rhs.p1.x);
+                return lhs_x_min < rhs_x_min;
+            }
+            return lhs_y_min < rhs_y_min;
+        }
     };
     const Self = @This();
     pub fn init(allocator: std.mem.Allocator) Self {
@@ -54,12 +64,12 @@ pub const Font = struct {
                             .y = outline.curves[j].p2.y,
                         },
                     });
-                    extra_pt = outline.curves[j].p2;
+                    extra_pt = Point{ .x = outline.curves[j].p2.x, .y = outline.curves[j].p2.y };
                 } else {
                     try edges.append(Edge{
                         .p0 = .{
-                            .x = extra_pt.x,
-                            .y = extra_pt.y,
+                            .x = extra_pt.?.x,
+                            .y = extra_pt.?.y,
                         },
                         .p1 = .{
                             .x = outline.curves[j].p0.x,
@@ -86,7 +96,8 @@ pub const Font = struct {
                             .y = outline.curves[j].p2.y,
                         },
                     });
-                    extra_pt = outline.curves[j].p2;
+                    extra_pt.?.x = outline.curves[j].p2.x;
+                    extra_pt.?.y = outline.curves[j].p2.y;
                 }
             }
             bezier_indx = outline.end_curves[i];
@@ -97,7 +108,8 @@ pub const Font = struct {
     //https://medium.com/@dillihangrae/scanline-filling-algorithm-852ad47fb0dd
     pub fn draw_char(self: *Self, graphics: anytype, character: u8) !void {
         const glyph_outline: ?TTF.GlyphOutline = self.ttf.char_map.get(character);
-        if (glyph_outline) |outline| {
+        if (glyph_outline != null) {
+            var outline: TTF.GlyphOutline = glyph_outline.?;
             const width = @as(u32, @bitCast(@as(i32, @intCast(outline.x_max - outline.x_min + 200))));
             const height = @as(u32, @bitCast(@as(i32, @intCast(outline.y_max - outline.y_min + 200))));
             var tex: *Texture = try self.allocator.create(Texture);
@@ -111,10 +123,23 @@ pub const Font = struct {
             std.debug.print("contour starts {any} {any} {any}\n", .{ outline.end_curves, outline.curves[outline.end_curves[0]], outline.curves[outline.end_curves[0] - 1] });
             var intersections: std.ArrayList(Point) = std.ArrayList(Point).init(self.allocator);
             var edges: std.ArrayList(Edge) = try self.gen_edges(&outline);
+            std.debug.print("before sort {any}\n", .{edges.items});
+            std.mem.sort(Edge, edges.items, {}, Edge.lessThan);
+            std.debug.print("after sort {any}\n", .{edges.items});
+            var active_edges: std.ArrayList(Edge) = std.ArrayList(Edge).init(self.allocator);
+            defer active_edges.deinit();
             defer edges.deinit();
             //TODO have edges now find which ones intersect then sort them and fill in between
             for (0..height) |i| {
                 _ = i;
+                //                 if(dy == 0) continue;
+
+                // f32 intersection = -1;
+                // if(dx == 0) {
+                //     intersection = edge->p1.x;
+                // } else {
+                //     intersection = (scanline - edge->p1.y)*(dx/dy) + edge->p1.x;
+                // }
                 // for (0..height) |i| {
                 //     intersections.clearRetainingCapacity();
                 //     for (0..width) |j| {
