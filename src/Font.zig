3c292f0e8ef39df791eb3bf2a9e591be3393c178
Timothy Cherney <tjcherney@hotmail.com>
2024-10-03 01:16:51 -0700

    fixed glyph rendering to take care of the edge cases, still need to work on how i want to size the glyphs along with smoothing them out

--- src/Font.zig
+++ src/Font.zig
@@ -18,7 +18,8 @@ pub const Font = struct {
     const Edge = struct {
         p0: Point,
         p1: Point,
-        pub fn lessThan(_: void, lhs: Edge, rhs: Edge) bool {
+        x: i32 = -1,
+        pub fn sort_y(_: void, lhs: Edge, rhs: Edge) bool {
             const lhs_y_min = @min(lhs.p0.y, lhs.p1.y);
             const rhs_y_min = @min(rhs.p0.y, rhs.p1.y);
             if (lhs_y_min == rhs_y_min) {
@@ -27,6 +28,9 @@ pub const Font = struct {
                 return lhs_x_min < rhs_x_min;
             }
             return lhs_y_min < rhs_y_min;
+        }
+        pub fn sort_x(_: void, lhs: Edge, rhs: Edge) bool {
+            return lhs.x < rhs.x;
         }
     };
     const Self = @This();
@@ -152,6 +156,7 @@ pub const Font = struct {
 
     //TODO scale curves to the eventual render size before processing instead of scaling image down as a final step
     //https://medium.com/@dillihangrae/scanline-filling-algorithm-852ad47fb0dd
+    //https://gabormakesgames.com/blog_polygons_scanline.html
     fn gen_char(self: *Self, graphics: anytype, character: u8) !*Texture {
         const glyph_outline: ?TTF.GlyphOutline = self.ttf.char_map.get(character);
         if (glyph_outline != null) {
@@ -170,50 +175,60 @@ pub const Font = struct {
             std.debug.print("curves {any}\n", .{outline.curves});
             var edges: std.ArrayList(Edge) = try self.gen_edges(&outline);
             std.debug.print("before sort {any}\n", .{edges.items});
-            std.mem.sort(Edge, edges.items, {}, Edge.lessThan);
+            std.mem.sort(Edge, edges.items, {}, Edge.sort_y);
             std.debug.print("after sort {any}\n", .{edges.items});
-            var intersections: std.ArrayList(i32) = std.ArrayList(i32).init(self.allocator);
+            var active_edges: std.ArrayList(Edge) = std.ArrayList(Edge).init(self.allocator);
+            var first_edge: usize = 0;
             for (0..height) |i| {
-                intersections.clearRetainingCapacity();
-                for (edges.items) |edge| {
-                    const larger_y = @max(edge.p0.y, edge.p1.y);
-                    const smaller_y = @min(edge.p0.y, edge.p1.y);
-                    if (@as(i32, @intCast(@as(i64, @bitCast(i)))) <= smaller_y or @as(i32, @intCast(@as(i64, @bitCast(i)))) > larger_y) continue;
-                    const dy: i32 = edge.p1.y - edge.p0.y;
-                    const dx: i32 = edge.p1.x - edge.p0.x;
-                    var intersection: i32 = undefined;
+                while (first_edge < edges.items.len) {
+                    const edge = edges.items[first_edge];
+                    if (@min(edge.p0.y, edge.p1.y) <= @as(i32, @intCast(@as(i64, @bitCast(i))))) {
+                        try active_edges.append(edge);
+                        first_edge += 1;
+                    } else {
+                        break;
+                    }
+                }
+                var j: usize = 0;
+                while (j < active_edges.items.len) {
+                    const edge = active_edges.items[j];
+                    if (@max(edge.p0.y, edge.p1.y) <= @as(i32, @intCast(@as(i64, @bitCast(i))))) {
+                        _ = active_edges.orderedRemove(j);
+                    } else {
+                        j += 1;
+                    }
+                }
+                for (0..active_edges.items.len) |k| {
+                    const dy: i32 = active_edges.items[k].p1.y - active_edges.items[k].p0.y;
+                    const dx: i32 = active_edges.items[k].p1.x - active_edges.items[k].p0.x;
                     if (dy == 0) continue;
                     if (dx == 0) {
-                        intersection = edge.p0.x;
+                        active_edges.items[k].x = active_edges.items[k].p0.x;
                     } else {
-                        intersection = @as(i32, @intFromFloat((@as(f32, @floatFromInt(i)) - @as(f32, @floatFromInt(edge.p0.y))) * (@as(f32, @floatFromInt(dx)) / @as(f32, @floatFromInt(dy))) + @as(f32, @floatFromInt(edge.p0.x))));
+                        active_edges.items[k].x = @as(i32, @intFromFloat((@as(f32, @floatFromInt(i)) - @as(f32, @floatFromInt(active_edges.items[k].p0.y))) * (@as(f32, @floatFromInt(dx)) / @as(f32, @floatFromInt(dy))) + @as(f32, @floatFromInt(active_edges.items[k].p0.x))));
                     }
-                    // our curves are defined with the previous end point as a part of it so the edges will overlap
-                    // var dupe: bool = false;
-                    // for (intersections.items) |j| {
-                    //     if (j == intersection) {
-                    //         dupe = true;
-                    //     }
-                    // }
-                    // if (!dupe)
-                    try intersections.append(intersection);
                 }
-                if (intersections.items.len <= 1) continue;
-                std.mem.sort(i32, intersections.items, {}, std.sort.asc(i32));
-                std.debug.print("intersections at {d} {any}\n", .{ i, intersections.items });
-                var j: usize = 0;
-                while (j < intersections.items.len) {
-                    if (j == intersections.items.len - 1) break;
-                    if (intersections.items[j] == intersections.items[j + 1]) {
-                        j += 1;
-                        continue;
+                if (active_edges.items.len < 1) continue;
+                std.mem.sort(Edge, active_edges.items, {}, Edge.sort_x);
+                var winding: i32 = 0;
+                var curr_edge: usize = 0;
+                for (@as(usize, @bitCast(@as(i64, @intCast(active_edges.items[0].x))))..@as(usize, @bitCast(@as(i64, @intCast(active_edges.items[active_edges.items.len - 1].x + 1))))) |x| {
+                    while (curr_edge != active_edges.items.len and active_edges.items[curr_edge].x < @as(i32, @intCast(@as(i64, @bitCast(x))))) {
+                        if (active_edges.items[curr_edge].p0.y <= @as(i32, @intCast(@as(i64, @bitCast(i)))) and active_edges.items[curr_edge].p1.y > @as(i32, @intCast(@as(i64, @bitCast(i))))) {
+                            winding -= 1;
+                        } else if (active_edges.items[curr_edge].p1.y <= @as(i32, @intCast(@as(i64, @bitCast(i)))) and active_edges.items[curr_edge].p0.y > @as(i32, @intCast(@as(i64, @bitCast(i))))) {
+                            winding += 1;
+                        }
+                        curr_edge += 1;
                     }
-                    graphics.draw_line(self.color, .{ .x = intersections.items[j], .y = @as(i32, @intCast(@as(i64, @bitCast(i)))) }, .{ .x = intersections.items[j + 1], .y = @as(i32, @intCast(@as(i64, @bitCast(i)))) }, tex.*);
-                    j += 2;
+
+                    if (winding != 0) {
+                        graphics.draw_pixel(@as(i32, @intCast(@as(i64, @bitCast(x)))), @as(i32, @intCast(@as(i64, @bitCast(i)))), self.color, tex.*);
+                    }
                 }
             }
-            intersections.deinit();
-
+            //graphics.draw_line(self.color, .{ .x = intersections.items[j], .y = @as(i32, @intCast(@as(i64, @bitCast(i)))) }, .{ .x = intersections.items[j + 1], .y = @as(i32, @intCast(@as(i64, @bitCast(i)))) }, tex.*);
+            active_edges.deinit();
             for (edges.items, 0..edges.items.len) |edge, j| {
                 if (j % 2 == 0) {
                     graphics.draw_line(.{ .r = 255, .g = 0, .b = 0 }, edge.p0, edge.p1, tex.*);
@@ -224,7 +239,7 @@ pub const Font = struct {
                 }
             }
             edges.deinit();
-            //try tex.scale(52, 96);
+            try tex.scale(52, 96);
             const f_name = ".bmp";
             var buff_name: [16]u8 = undefined;
             try tex.image_core().write_BMP(try std.fmt.bufPrint(&buff_name, "{d}{s}", .{ num_chars, f_name }));
