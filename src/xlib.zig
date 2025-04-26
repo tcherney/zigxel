a7253748d554e69997ae8a39b97ee570cd543a6e
Timothy Cherney <tjcherney@hotmail.com>
2025-04-25 21:31:29 -0700

    more x11 fiddling

--- src/xlib.zig
+++ src/xlib.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
-const c = @cImport({
-    @cInclude("X11/Xlib.h");
-});
+//TODO we may want to return to this idea, call everything from c then just build a wrapper around the event struct
+// const c = @cImport({
+//     @cInclude("X11/Xlib.h");
+// });
 
 pub const Display = struct {};
+pub const Window = struct {};
+pub const XEvent = struct {};
 
 pub extern fn XOpenDisplay(display_name: ?[*:0]const u8) *Display;
+pub extern fn XSelectInput(display: *Display, w: Window, event_mask: c_int) c_int;
+pub extern fn XNextEvent(display: *Display, event_return: *XEvent) c_int;
 
 //pub extern "C" fn XSelectInput(display: *Display, root_window: Window) i32;
 
