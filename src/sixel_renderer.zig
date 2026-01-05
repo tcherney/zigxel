872e61ed8027888135c99805fb0415d5589f5c57
Timothy Cherney <tjcherney@hotmail.com>
2026-01-05 15:23:34 -0800

    sixel planning

---
+++ src/sixel_renderer.zig
@@ -0,0 +1,2 @@
+//TODO https://www.digiater.nl/openvms/decus/vax90b1/krypton-nasa/all-about-sixels.text
+//TODO remove we wont actually need a seperate struct, will just be a different rendering function when flip is called
